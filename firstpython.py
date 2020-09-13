631ff0a3325f709905540a457e15124b4ce55be2
Lockelyy <63213139+Lockelyy@users.noreply.github.com>
2020-09-13 15:19:18 -0400

    Add files via upload

---
+++ firstpython.py
@@ -0,0 +1,25 @@
+import random
+
+randnumber = str(random.randint(1,20))
+print(randnumber)
+print("The computer has chosen a number between 1 and 20\nDo your best to guess what it is!")
+
+check = 0
+while check == 0:
+    userinput = input("Guess: ")
+    
+    if userinput == randnumber:
+        print("You have guessed the number!")
+        break
+
+    if userinput > randnumber:
+        print("You guessed too high! Try again")
+    
+    if userinput < randnumber:
+        print("You guessed too low! Try again")
+
+
+    
+
+
+
