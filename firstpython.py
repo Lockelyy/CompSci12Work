e9da7c60b9c2b4549461d5fb8fafd21f63f4cdda
Lockelyy <63213139+Lockelyy@users.noreply.github.com>
2020-09-17 20:49:23 -0400

    Update firstpython.py

--- firstpython.py
+++ firstpython.py
@@ -1,12 +1,13 @@
 import random
 
-randnumber = str(random.randint(1,20))
+randnumber = random.randint(1,20)
 print(randnumber)
 print("The computer has chosen a number between 1 and 20\nDo your best to guess what it is!")
 
 check = 0
 while check == 0:
     userinput = input("Guess: ")
+    userinput = int(userinput)
     
     if userinput == randnumber:
         print("You have guessed the number!")
@@ -17,9 +18,4 @@ while check == 0:
     
     if userinput < randnumber:
         print("You guessed too low! Try again")
-
-
-    
-
-
-
+       
